069bd205ae5ba87f22dacf793e74984951efb6b9
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-07-25 15:55:23 +0530

    added css and added some functionality

---
+++ to-do-list/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import App from "./App.css";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+  <div className="container">
+  <div className="app-wrapper">
+  <div>
+      <Header />
+  </div>
+  </div>  
+  </div>);
+}
+
+export default App;
